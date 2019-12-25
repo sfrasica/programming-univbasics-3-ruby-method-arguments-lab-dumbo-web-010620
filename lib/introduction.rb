057c625f47f5c2db32d2347ec91aacbba6b81e42
Steven Frasica <sfrasica1@gmail.com>
2019-12-25 23:21:11 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,11 +6,11 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction(name, language)
+def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction(name, language = "Ruby")
+def introduction_with_language_optional(name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
